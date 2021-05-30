ab339767428dbe9b2ee1959cd0f7ecb0990ba618
Artem <artmolla@gmail.com>
2021-05-31 00:57:12 +0300

    Добавляет футер - десктоп

--- build/js/main.js
+++ build/js/main.js
@@ -1,49 +1,49 @@
-'use strict';
+// 'use strict';
 
-(function () {
-  if (document.querySelector('.accordion')) {
+// (function () {
+//   if (document.querySelector('.accordion')) {
 
-    var accordionContent = document.querySelector('.accordion');
-    var accordionItems = Array.from(accordionContent.querySelectorAll('.accordion__item'));
-    var accordionButtons = Array.from(accordionContent.querySelectorAll('.accordion__button'));
+//     var accordionContent = document.querySelector('.accordion');
+//     var accordionItems = Array.from(accordionContent.querySelectorAll('.accordion__item'));
+//     var accordionButtons = Array.from(accordionContent.querySelectorAll('.accordion__button'));
 
-    var closeAllAccordion = function (element) {
-      var currentElement = element || null;
+//     var closeAllAccordion = function (element) {
+//       var currentElement = element || null;
 
-      accordionItems.forEach(function (it) {
-        if (it.classList.contains('accordion__item--open') && currentElement !== it) {
-          it.classList.remove('accordion__item--open');
-        }
-      });
-    };
+//       accordionItems.forEach(function (it) {
+//         if (it.classList.contains('accordion__item--open') && currentElement !== it) {
+//           it.classList.remove('accordion__item--open');
+//         }
+//       });
+//     };
 
-    var closeAccordionKeyPressHandler = function (evt) {
-      var isEscKey = evt.key === 'Escape' || evt.key === 'Esc';
+//     var closeAccordionKeyPressHandler = function (evt) {
+//       var isEscKey = evt.key === 'Escape' || evt.key === 'Esc';
 
-      if (isEscKey) {
-        evt.target.closest('.accordion__item').classList.remove('accordion__item--open');
-        evt.target.removeEventListener('keydown', closeAccordionKeyPressHandler);
-      }
-    };
+//       if (isEscKey) {
+//         evt.target.closest('.accordion__item').classList.remove('accordion__item--open');
+//         evt.target.removeEventListener('keydown', closeAccordionKeyPressHandler);
+//       }
+//     };
 
-    var toggleAccordionClickHandler = function (evt) {
-      evt.preventDefault();
+//     var toggleAccordionClickHandler = function (evt) {
+//       evt.preventDefault();
 
-      closeAllAccordion(evt.target.closest('.accordion__item'));
-      evt.target.closest('.accordion__item').classList.toggle('accordion__item--open');
-      evt.target.addEventListener('keydown', closeAccordionKeyPressHandler);
-    };
+//       closeAllAccordion(evt.target.closest('.accordion__item'));
+//       evt.target.closest('.accordion__item').classList.toggle('accordion__item--open');
+//       evt.target.addEventListener('keydown', closeAccordionKeyPressHandler);
+//     };
 
-    accordionButtons.forEach(function (it) {
-      it.classList.remove('accordion__button--no-js');
-      it.tabIndex = '0';
-      it.addEventListener('click', toggleAccordionClickHandler);
-    });
+//     accordionButtons.forEach(function (it) {
+//       it.classList.remove('accordion__button--no-js');
+//       it.tabIndex = '0';
+//       it.addEventListener('click', toggleAccordionClickHandler);
+//     });
 
-    closeAllAccordion();
+//     closeAllAccordion();
 
-  }
-})();
+//   }
+// })();
 
 'use strict';
 
